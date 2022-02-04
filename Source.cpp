3366eda5374ffc4e0eed1e1de3dca5a8cfb1646b
ali arman <56387750+ali110arman@users.noreply.github.com>
2022-02-04 18:45:38 +0330

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void main() {
+	int a, b;
+	cout << "inter first number:\n";
+	cin >> a;
+	cout << "inter secoonnd number:\n";
+	cin >> b;
+	cout << "your numbers sum:"<< a + b;
+}
